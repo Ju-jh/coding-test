40156494099bd1701874cf8892074903fa6e6447
Jayden Ju (주재훈) <jjdong9108@gmail.com>
2024-02-27 17:45:32 +0900

    'gitignore_edit'

--- 11. 시간복잡도/24262.js
+++ 11. 시간복잡도/24262.js
@@ -20,3 +20,13 @@ MenOfPassion(A[], n) {
 첫째 줄에 코드1 의 수행 횟수를 출력한다.
 
 둘째 줄에 코드1의 수행 횟수를 다항식으로 나타내었을 때, 최고차항의 차수를 출력한다. 단, 다항식으로 나타낼 수 없거나 최고차항의 차수가 3보다 크면 4를 출력한다. */
+
+const n = Number(require('fs').readFileSync('example.txt').toString().trim());
+const array = [];
+
+function MenOfPassion(array, n) {
+  let i = n / 2;
+  return array[i];
+}
+
+MenOfPassion(array, n);
