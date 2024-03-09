eca1973db09dbf1e5005429e4c6bf9897f64bdc2
Jayden Ju (주재훈) <jjdong9108@gmail.com>
2024-03-09 23:18:05 +0900

    'uncompleted_grade_eleven'

--- 11. 시간복잡도/24262.js
+++ 11. 시간복잡도/24262.js
@@ -21,7 +21,9 @@ MenOfPassion(A[], n) {
 
 둘째 줄에 코드1의 수행 횟수를 다항식으로 나타내었을 때, 
 
-최고차항의 차수를 출력한다. 단, 다항식으로 나타낼 수 없거나 최고차항의 차수가 3보다 크면 4를 출력한다. */
+최고차항의 차수를 출력한다. 단, 다항식으로 나타낼 수 없거나 최고차항의 차수가 3보다 크면 4를 출력한다. 
+
+*/
 
 const n = Number(require('fs').readFileSync('example.txt').toString().trim());
 const array = [];
